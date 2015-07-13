1333f4afbb50fe6f29c88635cdfa3e1e883e9841
Sven Günther <sven.guenther@it-agile.de>
2015-07-13 18:55:17 +0200

    remove test impl

--- src/test/java/com/itagile/tddcamp/terminkalender/KalenderTest.java
+++ src/test/java/com/itagile/tddcamp/terminkalender/KalenderTest.java
@@ -9,23 +9,10 @@ public class KalenderTest {
 	private Kalender kalender = new Kalender();
 	private Termin termin = new Termin();
 
-	@Test
-	public void traegtTerminEin() {
-		kalender.trageEin(termin);
-		assertTrue(kalender.hat(termin));
-	}
+	// termin eintragen
 
-	@Test
-	public void traegtBesitzerInTerminEin() throws Exception {
-		Teilnehmer teilnehmer = new Teilnehmer();
-		kalender.setBesitzer(teilnehmer);
-		kalender.trageEin(termin);
-		assertTrue(termin.nimmtTeil(teilnehmer));
-	}
-	
-	@Test(expected=TerminDoppeltEingetragenException.class)
-	public void schmeisstExceptionWennTerminDoppeltEingetragenWird() throws Exception {
-		kalender.trageEin(termin);
-		kalender.trageEin(termin);
-	}
+	// Besitzer in Termin eintragen
+
+
+	// schmeisst Exception wenn Termin doppelt eingetragen wird
 }
--- src/test/java/com/itagile/tddcamp/terminkalender/ZeitUtilTest.java
+++ src/test/java/com/itagile/tddcamp/terminkalender/ZeitUtilTest.java
@@ -12,31 +12,9 @@ import org.junit.Test;
 
 public class ZeitUtilTest {
 
-	Calendar kalender = Calendar.getInstance();
-	
-	@Test
-	public void formatiertDateNachString() throws Exception {
-		kalender.set(Calendar.HOUR_OF_DAY, 23);
-		kalender.set(Calendar.MINUTE, 59);
-		Date zeit = kalender.getTime();
-		assertEquals("23:59", ZeitUtil.formatiereZeit(zeit));
-	}
-	
-	@Test
-	public void parstStringNachDate() throws Exception {
-		Date zeit = ZeitUtil.parseZeit("23:59");
-		kalender.setTime(zeit);
-		assertEquals(23, kalender.get(Calendar.HOUR_OF_DAY));
-		assertEquals(59, kalender.get(Calendar.MINUTE));
-	}
-	
-	@Test
-	public void wandeltParseExceptionNachRuntimeExceptionUm() throws Exception {
-		try {
-			ZeitUtil.parseZeit("unparsbar");
-			fail();
-		} catch (RuntimeException exception) {
-			assertTrue(exception.getCause() instanceof ParseException);
-		}
-	}
+	// Datum (Zeit) nach String formatieren (23:59)
+
+	// Aus String Datum (Zeit) parsen
+
+	// wandelt ParseException in RuntimeException um
 }
